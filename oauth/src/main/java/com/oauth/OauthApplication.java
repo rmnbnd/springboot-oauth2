0ead2fd9a8a4967f543357a85d15eddeedf948fa
Roman Bondarchuk <rmnbnd@gmail.com>
2016-08-12 00:27:38 +0300

    Setup oauth application

--- oauth/src/main/java/com/oauth/OauthApplication.java
+++ oauth/src/main/java/com/oauth/OauthApplication.java
@@ -1,12 +1,183 @@
 package com.oauth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SpringBootApplication
-public class OauthApplication {
+public class OauthApplication extends SpringBootServletInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(OauthApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(OauthApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OauthApplication.class);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        @Autowired // <-- This is crucial otherwise Spring Boot creates its own
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            log.info("Defining inMemoryAuthentication (2 users)");
+            auth
+                    .inMemoryAuthentication()
+
+                    .withUser("user").password("password")
+                    .roles("USER")
+
+                    .and()
+
+                    .withUser("admin").password("password")
+                    .roles("USER", "ADMIN")
+            ;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.formLogin()
+                    .and()
+                    .httpBasic().disable()
+                    .anonymous().disable()
+                    .authorizeRequests().anyRequest().authenticated()
+            ;
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+        private String privateKey = "            -----BEGIN RSA PRIVATE KEY-----\n" +
+                "            MIICXQIBAAKBgQDNQZKqTlO/+2b4ZdhqGJzGBDltb5PZmBz1ALN2YLvt341pH6i5\n" +
+                "            mO1V9cX5Ty1LM70fKfnIoYUP4KCE33dPnC7LkUwE/myh1zM6m8cbL5cYFPyP099t\n" +
+                "            hbVxzJkjHWqywvQih/qOOjliomKbM9pxG8Z1dB26hL9dSAZuA8xExjlPmQIDAQAB\n" +
+                "            AoGAImnYGU3ApPOVtBf/TOqLfne+2SZX96eVU06myDY3zA4rO3DfbR7CzCLE6qPn\n" +
+                "            yDAIiW0UQBs0oBDdWOnOqz5YaePZu/yrLyj6KM6Q2e9ywRDtDh3ywrSfGpjdSvvo\n" +
+                "            aeL1WesBWsgWv1vFKKvES7ILFLUxKwyCRC2Lgh7aI9GGZfECQQD84m98Yrehhin3\n" +
+                "            fZuRaBNIu348Ci7ZFZmrvyxAIxrV4jBjpACW0RM2BvF5oYM2gOJqIfBOVjmPwUro\n" +
+                "            bYEFcHRvAkEAz8jsfmxsZVwh3Y/Y47BzhKIC5FLaads541jNjVWfrPirljyCy1n4\n" +
+                "            sg3WQH2IEyap3WTP84+csCtsfNfyK7fQdwJBAJNRyobY74cupJYkW5OK4OkXKQQL\n" +
+                "            Hp2iosJV/Y5jpQeC3JO/gARcSmfIBbbI66q9zKjtmpPYUXI4tc3PtUEY8QsCQQCc\n" +
+                "            xySyC0sKe6bNzyC+Q8AVvkxiTKWiI5idEr8duhJd589H72Zc2wkMB+a2CEGo+Y5H\n" +
+                "            jy5cvuph/pG/7Qw7sljnAkAy/feClt1mUEiAcWrHRwcQ71AoA0+21yC9VkqPNrn3\n" +
+                "            w7OEg8gBqPjRlXBNb00QieNeGGSkXOoU6gFschR22Dzy\n" +
+                "            -----END RSA PRIVATE KEY-----";
+
+        private String publicKey = "            -----BEGIN PUBLIC KEY-----\n" +
+                "            MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDNQZKqTlO/+2b4ZdhqGJzGBDlt\n" +
+                "            b5PZmBz1ALN2YLvt341pH6i5mO1V9cX5Ty1LM70fKfnIoYUP4KCE33dPnC7LkUwE\n" +
+                "            /myh1zM6m8cbL5cYFPyP099thbVxzJkjHWqywvQih/qOOjliomKbM9pxG8Z1dB26\n" +
+                "            hL9dSAZuA8xExjlPmQIDAQAB\n" +
+                "            -----END PUBLIC KEY-----";
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        public JwtAccessTokenConverter tokenEnhancer() {
+            log.info("Initializing JWT with public key:\n" + publicKey);
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            converter.setSigningKey(privateKey);
+            converter.setVerifierKey(publicKey);
+            return converter;
+        }
+
+        @Bean
+        public JwtTokenStore tokenStore() {
+            return new JwtTokenStore(tokenEnhancer());
+        }
+
+        /**
+         * Defines the security constraints on the token endpoints /oauth/token_key and /oauth/check_token
+         * Client credentials are required to access the endpoints
+         *
+         * @param oauthServer
+         * @throws Exception
+         */
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer
+                    .tokenKeyAccess("isAnonymous() || hasRole('ROLE_TRUSTED_CLIENT')") // permitAll()
+                    .checkTokenAccess("hasRole('TRUSTED_CLIENT')"); // isAuthenticated()
+        }
+
+        /**
+         * Defines the authorization and token endpoints and the token services
+         *
+         * @param endpoints
+         * @throws Exception
+         */
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+
+                    // Which authenticationManager should be used for the password grant
+                    // If not provided, ResourceOwnerPasswordTokenGranter is not configured
+                    .authenticationManager(authenticationManager)
+
+                    // Use JwtTokenStore and our jwtAccessTokenConverter
+                    .tokenStore(tokenStore())
+                    .accessTokenConverter(tokenEnhancer())
+            ;
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+
+                    // Confidential client where client secret can be kept safe (e.g. server side)
+                    .withClient("confidential").secret("secret")
+                    .authorizedGrantTypes("client_credentials", "authorization_code", "refresh_token")
+                    .scopes("read", "write")
+                    .redirectUris("http://localhost:8080/client/")
+
+                    .and()
+
+                    // Public client where client secret is vulnerable (e.g. mobile apps, browsers)
+                    .withClient("public") // No secret!
+                    .authorizedGrantTypes("client_credentials", "implicit")
+                    .scopes("read")
+                    .redirectUris("http://localhost:8080/client/")
+
+                    .and()
+
+                    // Trusted client: similar to confidential client but also allowed to handle user password
+                    .withClient("trusted").secret("secret")
+                    .authorities("ROLE_TRUSTED_CLIENT")
+                    .authorizedGrantTypes("client_credentials", "password", "authorization_code", "refresh_token")
+                    .scopes("read", "write")
+                    .redirectUris("http://localhost:8080/client/")
+            ;
+        }
+
+    }
+
 }
