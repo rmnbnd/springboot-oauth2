94688c86b21b1cf029c8f01a7b895535af9622c1
Roman Bondarchuk <rmnbnd@gmail.com>
2016-08-12 00:54:52 +0300

    Setup client application

--- client/src/main/java/com/client/ClientApplication.java
+++ client/src/main/java/com/client/ClientApplication.java
@@ -1,12 +1,62 @@
 package com.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class ClientApplication {
+@EnableOAuth2Client
+public class ClientApplication extends SpringBootServletInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
+
+    @Value("${config.oauth2.accessTokenUri}")
+    private String accessTokenUri;
+
+    @Value("${config.oauth2.userAuthorizationUri}")
+    private String userAuthorizationUri;
+
+    @Value("${config.oauth2.clientID}")
+    private String clientID;
+
+    @Value("${config.oauth2.clientSecret}")
+    private String clientSecret;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClientApplication.class);
+    }
+
+    @Bean
+    public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(resource(), oauth2ClientContext);
+    }
+
+    private OAuth2ProtectedResourceDetails resource() {
+        AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+        resource.setClientId(clientID);
+        resource.setClientSecret(clientSecret);
+        resource.setAccessTokenUri(accessTokenUri);
+        resource.setUserAuthorizationUri(userAuthorizationUri);
+        resource.setScope(Arrays.asList("read"));
+
+        return resource;
     }
 }
