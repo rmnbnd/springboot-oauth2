6a75c1e21d7aec163c8b6d297741c3ec972e6366
Roman Bondarchuk <rmnbnd@gmail.com>
2016-08-12 03:20:13 +0300

    Refactor resource

--- resource/src/main/java/com/resource/ResourceApplication.java
+++ resource/src/main/java/com/resource/ResourceApplication.java
@@ -1,45 +1,16 @@
 package com.resource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 public class ResourceApplication extends SpringBootServletInitializer {
 
-    private static final Logger log = LoggerFactory.getLogger(ResourceApplication.class);
-
-
     public static void main(String[] args) {
         SpringApplication.run(ResourceApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ResourceApplication.class);
-    }
-
-    @Configuration
-    @EnableWebSecurity
-    @EnableGlobalMethodSecurity(prePostEnabled = true)
-    protected static class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
-        }
-    }
 }
