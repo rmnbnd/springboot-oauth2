d58e9576db1f07eb7480a71d2431848a231e70b3
Roman Bondarchuk <rmnbnd@gmail.com>
2016-08-16 20:52:53 +0300

    Update token enhancer

--- oauth/src/main/java/com/oauth/CustomTokenEnhancer.java
+++ oauth/src/main/java/com/oauth/CustomTokenEnhancer.java
@@ -15,8 +15,7 @@ class CustomTokenEnhancer implements TokenEnhancer {
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         final Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("authorities", user.getAuthorities());
-
+        additionalInfo.put("roles", user.getAuthorities());
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
